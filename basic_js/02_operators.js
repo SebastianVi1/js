5ed64062e9e200c5c08dccb1aa4f8502560a6412
sebas <sebastianvh86@gmail.com>
2025-05-17 13:21:44 -0600

    operators

---
+++ basic_js/02_operators.js
@@ -0,0 +1,29 @@
+let a = 10;
+let b = 3;
+
+console.log(a + b);  // 13
+console.log(a - b);  // 7
+console.log(a * b);  // 30
+console.log(a / b);  // 3.333...
+console.log(a % b);  // 1
+console.log(a ** b); // 1000
+
+//Assigment operators
+
+console.log(a > b); // true
+console.log(a < b); // false
+console.log(a >= b) // true
+console.log(a== b); // false
+console.log(a === b); // false
+
+//Increment y decrement operators
+
+console.log(a++) // Increment after using the variable
+console.log(++a) //Increment before using the variable
+
+console.log(a--);
+console.log(--a);
+
+// Logic operators
+console.log(a && b);
+console.log(a || b);
