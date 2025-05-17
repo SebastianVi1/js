b60b9298bb0aa65dce68bdd2ba54bbf5a44a5431
sebas <sebastianvh86@gmail.com>
2025-05-17 13:25:59 -0600

    objects & arrays

--- basic_js/04_objects&arrays.js
+++ basic_js/04_objects&arrays.js
@@ -1,2 +1,15 @@
 // Making an object
+let car = {
+    color: 'red',
+    tires : 4,
+    doors : 2,
+    drive : function(){return "drive"}
+};
+
+// Create an array
+let shoppingList = [
+    'apple',
+    'orange',
+    'pear',
+];
 
