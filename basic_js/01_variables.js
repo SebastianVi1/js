6195fe28992e800de0409cb1bfc22a898063f16b
sebas <sebastianvh86@gmail.com>
2025-05-17 13:21:32 -0600

    variables

---
+++ basic_js/01_variables.js
@@ -0,0 +1,9 @@
+let nombre = 'sebastian'; //Mutable data
+const lastName = 'Villarreal'; //Inmutable data
+console.log(nombre); //Print data on console
+
+nombre = "Sebastion";
+lastName = 'Heredia';// Not possible
+
+console.log(nombre);
+console.log(lastName);
