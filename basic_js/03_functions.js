aaabb44d0996f39310b9b22f69e61206f1967963
sebas <sebastianvh86@gmail.com>
2025-05-17 13:21:57 -0600

    functions

---
+++ basic_js/03_functions.js
@@ -0,0 +1,13 @@
+// function function_name(parameters) { block, return}
+function makeCoffe(sugar, milk) {
+    let instructions = "boild water,";
+    instructions+= "pour into cup,";
+    return instructions;
+}
+
+function addTwoNumbers(number1, number2) {
+    return number1 + number2;
+
+}
+
+document.writeln(addTwoNumbers(1, 1));
---
+++ basic_js/04_objects&arrays.js
@@ -0,0 +1,2 @@
+// Making an object
+
