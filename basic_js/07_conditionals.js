9fbc27722c76d90aa631a4f6c32aa4f0d05c0dc7
Andre <sebastianvh86@gmail.com>
2025-05-17 18:41:06 -0600

    if else..

---
+++ basic_js/07_conditionals.js
@@ -0,0 +1,18 @@
+// if (condition) {block} else {block}
+
+let a = 10;
+let b = 100;
+
+if (a > b){
+    console.log("a is bigger");
+} else if (a == b) {
+    console.log('same number');
+}else {
+    console.log('a is minor');
+}
+
+// conditional simplified
+
+//if (condition) code to execute if condicion is (true) else then execute this
+
+if (a  < b ) console.log("a is minor"); else console.log('hello');
