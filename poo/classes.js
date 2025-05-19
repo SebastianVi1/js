8b6324f05d06b99323f61f90f40165ae9207ae62
Andre <sebastianvh86@gmail.com>
2025-05-19 13:50:57 -0600

    classes

---
+++ poo/classes.js
@@ -0,0 +1,35 @@
+// -------------------------------------------------------
+function Dog(name, age){
+    let dog = Object.create(dogConstructor);
+    dog.name = name;
+    dog.age = age;
+
+    return dog;
+}
+//       How classes work
+
+let dogConstructor =  {
+    talk : function() {
+        return 'guau'
+    }
+}
+let dog = Dog('Sanson', 12);
+console.log(dog.talk);
+// ouput guau
+// ---------------------------------------------------------
+
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+    
+    name() {
+        console.log(this.name);
+    }
+
+
+}
+
+const person = new Person('sebastian');
+console.log(person.name);
+
