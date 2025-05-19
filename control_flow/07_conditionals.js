7c8886def9279bda72e840173191fab766e8cf21
sebas <sebastianvh86@gmail.com>
2025-05-19 08:35:56 -0600

    conditionals

---
+++ control_flow/07_conditionals.js
@@ -0,0 +1,34 @@
+// if (condition) {block} else {block}
+
+let a = 10;
+let b = 100;
+
+if (a > b){
+    console.log("a is bigger");
+} else if (a == b) {
+    console.log('same number');
+}else {
+    console.log('a is minor');
+}
+
+// conditional simplified
+
+//if (condition) code to execute if condicion is (true) else then execute this
+
+if (a  < b ) console.log("a is minor"); else console.log('hello');
+
+//Switch case 
+
+const expr = "Papayas";
+switch (expr) {
+  case "Oranges":
+    console.log("Oranges are $0.59 a pound.");
+    break;
+  case "Mangoes":
+  case "Papayas":
+    console.log("Mangoes and papayas are $2.79 a pound.");
+    // Expected output: "Mangoes and papayas are $2.79 a pound."
+    break;
+  default:
+    console.log(`Sorry, we are out of ${expr}.`);
+}
