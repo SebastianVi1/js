467241bab99b8f01a7593aaafdbe0e849462aef5
sebas <sebastianvh86@gmail.com>
2025-05-19 08:01:39 -0600

    conditionals

--- basic_js/07_conditionals.js
+++ basic_js/07_conditionals.js
@@ -16,3 +16,19 @@ if (a > b){
 //if (condition) code to execute if condicion is (true) else then execute this
 
 if (a  < b ) console.log("a is minor"); else console.log('hello');
+
+//Switch case 
+
+const expr = "Papayas";
+switch (expr) {
+  case "Oranges":
+    console.log("Oranges are $0.59 a pound.");
+    break;
+  case "Mangoes":
+  case "Papayas":
+    console.log("Mangoes and papayas are $2.79 a pound.");
+    // Expected output: "Mangoes and papayas are $2.79 a pound."
+    break;
+  default:
+    console.log(`Sorry, we are out of ${expr}.`);
+}
